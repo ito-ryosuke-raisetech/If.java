5e38c8cb07c2b71dbf723b09f00853b785a0574a
ito-ryosuke-raisetech <85591560+ito-ryosuke-raisetech@users.noreply.github.com>
2021-06-17 11:05:59 +0900

    Create If.java

---
+++ If.java
@@ -0,0 +1,16 @@
+package If;
+
+public class If {
+
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+	    int num2 = 1;
+	    if (num1 < 5 ||  num2 < 20 ) {
+        System.out.println("引数が０なら偽。");
+    } {
+        System.out.println("引数が１なら真");
+     }
+	}
+
+}
